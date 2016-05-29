b31ffa46fd8a1dac31df3989ffea8b43001dfa36
Muhamad Ishlah <nurul.ishlah@gmail.com>
2016-05-29 10:00:37 +1000

    created reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,50 @@
 import { combineReducers } from 'redux';
+import {
+  FETCH_REGION, REQUEST_C1_RECAP, RECEIVE_C1_RECAP
+} from '../actions/index';
+
+
+function selectedRegion(state = '', action) {
+  switch (action.type) {
+    case FETCH_REGION:
+      return action.region;
+    default:
+      return state;
+  }
+}
+
+function regions(state = {
+  isFetching: false,
+  recapitulation: []
+}, action) {
+  switch(action.type) {
+    case REQUEST_C1_RECAP:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case RECEIVE_C1_RECAP:
+      return Object.assign({}, state, {
+        isFetching: false,
+        recapitulation: action.recapitulation 
+      });
+    default:
+      return state;
+  }
+}
+
+function regionsRecapitulation(state = {}, action) {
+  if (action.type === REQUEST_C1_RECAP) {
+    return Object.assign({}, stae, {
+      [action.region]: regions(state[action.region], action)
+    });
+  }
+
+  return state;
+}
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  selectedRegion,
+  regionsRecapitulation
 });
 
 export default rootReducer;
